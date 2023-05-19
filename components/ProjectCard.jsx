9c468969e62ec8ba564090763b0b8030fb0ea8e8
mrwebwork <tech@allanswebwork.info>
2023-05-19 04:42:10 -0500

    Add attribute for security reasons

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -18,6 +18,7 @@ export default function ProjectCard({
         <a
           href={href}
           target="_blank"
+          rel="noopener noreferrer"
           className="px-4 py-2 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"
         >
           {linkText}

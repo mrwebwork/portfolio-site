dde1ebf88534c599885c244833b8282358bd3d03
mrwebwork <tech@allanswebwork.info>
2023-05-22 09:33:07 -0500

    Add Head imports

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Head from "next/head";
 import Navbar from "@/components/Navbar";
 import Header from "@/components/Header";
 import ContactForm from "@/components/ContactForm";

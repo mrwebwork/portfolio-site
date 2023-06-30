5d5c4a2dabc9a1e24df398ae98b69c1241eb0277
mrwebwork <tech@allanswebwork.info>
2023-06-30 17:38:39 -0500

    Optimize theme toggler componenet to switch on every click

--- components/ToggleTheme.jsx
+++ components/ToggleTheme.jsx
@@ -1,52 +1,46 @@
-//* Importing required modules and components
 import { useEffect, useState } from "react";
 import { FaSun, FaMoon } from "react-icons/fa";
 
-//* The ToggleTheme component is responsible for handling theme switching functionality.
 export default function ToggleTheme() {
-  //* The 'isDark' state determines the current theme. It initially tries to get the theme from local storage or defaults to 'false' (light theme).
   const [isDark, setIsDark] = useState(() => {
-    //* Checking if window is defined before trying to access local storage to avoid issues during server side rendering.
     if (typeof window !== "undefined") {
-      return window.localStorage.getItem("theme") === "dark";
+      const storedTheme = window.localStorage.getItem("theme");
+      if (storedTheme) {
+        return storedTheme === "dark";
+      }
+      return null;
     } else {
       return false;
     }
   });
 
-  //* 'useEffect' hook is used to add or remove the 'dark' class to the document element depending on the 'isDark' state, and also save the current theme to local storage.
   useEffect(() => {
-    if (isDark) {
-      document.documentElement.style.setProperty(
-        "--foreground",
-        "var(--color-primary-light)"
-      );
-      document.documentElement.style.setProperty(
-        "--background",
-        "var(--color-primary)"
-      );
-      document.documentElement.classList.add("dark");
-      window.localStorage.setItem("theme", "dark");
-    } else {
-      document.documentElement.style.setProperty(
-        "--foreground",
-        "var(--color-primary)"
-      );
-      document.documentElement.style.setProperty(
-        "--background",
-        "var(--color-primary-light)"
-      );
-      document.documentElement.classList.remove("dark");
-      window.localStorage.setItem("theme", "light");
+    if (isDark === null) {
+      return;
     }
-  }, [isDark]); //* 'isDark' state is added as a dependency to 'useEffect' hook, meaning it will run everytime 'isDark' changes.
 
-  //* 'toggleTheme' function is responsible for switching the 'isDark' state value, effectively toggling the theme.
+    const foregroundColor = isDark
+      ? "var(--color-primary-light)"
+      : "var(--color-primary)";
+    const backgroundColor = isDark
+      ? "var(--color-primary)"
+      : "var(--color-primary-light)";
+    const theme = isDark ? "dark" : "light";
+
+    document.documentElement.style.setProperty("--foreground", foregroundColor);
+    document.documentElement.style.setProperty("--background", backgroundColor);
+    document.documentElement.classList.toggle("dark", isDark);
+    window.localStorage.setItem("theme", theme);
+  }, [isDark]);
+
   const toggleTheme = () => {
-    setIsDark(!isDark);
+    setIsDark((prevIsDark) => !prevIsDark);
   };
 
-  //* Depending on the 'isDark' state, different icons are assigned to the 'icon' variable.
+  if (isDark === null) {
+    return null;
+  }
+
   const icon = isDark ? <FaSun size={25} /> : <FaMoon size={25} />;
 
   return (
